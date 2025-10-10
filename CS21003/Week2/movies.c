582f7f9c5eb6b689ebcbd0b78dc9e4d696fd49ba
milespallett <150683238+milespallett@users.noreply.github.com>
2025-10-10 11:52:44 +0100

    Task 7 completed

--- CS21003/Week2/movies.c
+++ CS21003/Week2/movies.c
@@ -22,14 +22,35 @@ void displayMovieInformation1(movieInfo *infoToDisplay){
     return;
 }
 
+void displayMovieArray(movieInfo arrayToDisplay[], int size){
+    for (int i=0; i<size; i++){
+        printf("\nMovie %d\nName: %s\nRuntime: %d\nRating: %.1lf\n", i+1, arrayToDisplay[i].movieName, arrayToDisplay[i].movieRunTime, arrayToDisplay[i].movieRating);
+    }
+    return;
+}
+
 int main()
 {
-    movieInfo userMovie;
 
     char userMovieName[150];
     int userMovieRunTime;
     double userMovieRating;
 
+    movieInfo userMovieArray[3];
+
+    for (int i=0; i<3; i++){
+        printf("Movie name (Input in camel case): ");
+        scanf("%s", &userMovieArray[i].movieName);
+        printf("Movie runtime (minutes): ");
+        scanf("%d", &userMovieArray[i].movieRunTime);
+        printf("Movie rating: ");
+        scanf("%lf", &userMovieArray[i].movieRating);
+    }
+
+    displayMovieArray(userMovieArray, (sizeof(userMovieArray)/sizeof(userMovieArray[0])));
+
+    /*
+    movieInfo userMovie;
     printf("Movie name: ");
     scanf("%s", &userMovieName);
     printf("Movie runtime (minutes): ");
@@ -43,6 +64,7 @@ int main()
     (*pMovieInfo).movieRating = userMovieRating;
 
     displayMovieInformation1(pMovieInfo);
+    */
 
     return 0;
 }
