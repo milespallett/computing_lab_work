4dd998c7e48ba62cf7022a6c0f2470c3cf83be8b
milespallett <2584601@dundee.ac.uk>
2026-07-16 23:45:38 +0100

    Created model using a2c policy

--- CS21002/Resit/model.py
+++ CS21002/Resit/model.py
@@ -1,5 +1,72 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
+##Code adapted from article:https://www.geeksforgeeks.org/deep-learning/reinforcement-learning-using-pytorch/
+##Along with the stable-baseline3 documentation pages https://stable-baselines3.readthedocs.io/en/master/modules/a2c.html and https://stable-baselines3.readthedocs.io/en/master/guide/quickstart.html
+###Imports###
+from MariNav.Env.MariNav import MariNav
+import networkx as nx
+import pandas as pd
+from stable_baselines3 import A2C
+from stable_baselines3.common.logger import configure
 
+###Variable Delcarations###
+#pairs taken from brief on mydundee
+PAIRS = [('861909047ffffff','861956917ffffff')
+         , ('861909047ffffff', '861956c77ffffff')
+         , ('861956327ffffff', '86195665fffffff')
+         , ('861956327ffffff', '861956917ffffff')
+         , ('861956327ffffff', '861956c77ffffff')]
+WIND_MAP_PATH = 'CS21002\\North_Channel_windmap.csv' #path to windmap csv file provided on mydundee
+GRAPH_PATH = 'CS21002\\North_Channel_visits.gexf' #path to gexf file provided on mydundee
+
+
+###Logger setup
+tmp_path = "/tmp/sb3_log"
+new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+
+
+###Functions###
+#Based on https://gist.github.com/joaofig/c5123d2d36206aa85104f838eec896d8 -- UNUSED FUNCTION --
+'''def graph_from_hexes(hexes):
+    g = nx.Graph()
+    for i in hexes:
+        g.add_node(i)
+    return g'''
+
+
+###Main###
+
+#create marinav environment
+def create_env():
+    env = MariNav(
+        graph=nxGraph,
+        wind_map=windmapDict,
+        pairs=PAIRS
+    )
+    print("Environment created")
+    return env
+
+
+if __name__ == "__main__":
+    print(f"Creating array of h3 cells from {GRAPH_PATH}...")
+    #array of h3 cells, using networkx to extract nodes from gexf file provided on mydundee
+    h3Array = nx.read_gexf('CS21002\\North_Channel_visits.gexf').nodes
+
+    print(f"Creating windmap dictionary from {WIND_MAP_PATH}... ")
+    #dictionary created from windmap csv file provided on mydundee, using pandas to read csv and convert to dictionary
+    windmapDict = pd.read_csv('CS21002\\North_Channel_windmap.csv')
+    windmapDict = pd.DataFrame.to_dict(windmapDict)
+
+    print(f"Loading graph from {GRAPH_PATH}...")
+    #networkx graph created from gexf file provided on mydundee, using networkx to read gexf file and convert to undirected graph
+    nxGraph = nx.read_gexf('CS21002\\North_Channel_visits.gexf').to_undirected()
+
+    print(f"Creating environment...")
+    mnEnv = create_env()
+
+    #Run and log model using A2C RL algorithm and stable-baseline3's MLP model policy
+    model = A2C("MlpPolicy", mnEnv)
+    model.set_logger(new_logger)
+    model.learn(10_000)
+
+    #prints the current model policy for checking
+    print("\n Outline of the policy used:")
+    print(model.policy)
